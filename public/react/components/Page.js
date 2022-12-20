7647a8e7eb42efdf9cdc940777303c5f67861c62
miavh123 <miahug123@gmail.com>
2022-12-20 18:24:19 -0500

    inital commit

--- public/react/components/Page.js
+++ public/react/components/Page.js
@@ -1,9 +1,28 @@
 import React from 'react';
 
-export const Page = (props) => {
 
+export const Page = (props) => {
+ 
+  const [dataToDisplay, setDataToDisplay] = useState(undefined);
+
+  const handleClick = async () => {
+    console.log("Clicked!!")
+    // just get data for a SINGLE article
+    const response = await fetch(`${apiURL}/wiki/${props.page.slug}`);
+    const data = await response.json();
+    console.log(data);
+    // update the state
+    setDataToDisplay(data);
+  }
   return <>
-    <h3>{props.page.title}</h3>
-  </>
+  <h3 className="headline" 
+    onClick={ handleClick }>{props.page.title}</h3>
+    <div className="dataDump">
+      {
+        // render data
+        dataToDisplay ? dataToDisplay.author.name : ""
+      }
+    </div>
+</>
 } 
 	
